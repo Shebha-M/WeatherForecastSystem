0deb44e96c7c5cce21f0fbae687bec822d11977e
Shebha-M <shebham.06@gmail.com>
2026-06-29 22:52:15 +0530

    Removed database credentials

--- src/main/java/com/wipro/weather/util/DBUtil.java
+++ src/main/java/com/wipro/weather/util/DBUtil.java
@@ -13,10 +13,10 @@ public class DBUtil
         	Class.forName("oracle.jdbc.OracleDriver");
         	
             con = DriverManager.getConnection(
-            	"jdbc:oracle:thin:@(description=(address=(protocol=tcps)(port=2484)(host=db.freesql.com))(connect_data=(service_name=23ai_34ui2)))",
-                "SHEBHAM_06_SCHEMA_4EGKF",
-                "AEZ7F5H56ZY#NE8PrYQLEN8LJ6PEMX"
-            );
+    "jdbc:oracle:...",
+    "YOUR_USERNAME",
+    "YOUR_PASSWORD"
+);
             System.out.println("DB Connected Successfully!");
 
         } 
